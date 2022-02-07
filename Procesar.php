164091e059684295780cc0e02cd691ae070ea8a4
Lempi4K <lemopi2008@gmail.com>
2022-02-07 13:42:59 -0700

    fotos.html v2

---
+++ Procesar.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html> 
+<html lang="es"> 
+<head> 
+  <meta charset="utf-8"> 
+  <title>Respuesta</title> 
+</head> 
+<body> 
+  <section> 
+    <p> 
+      <?php 
+        print('El valor es: '.$_GET['user']); 
+      ?> 
+    </p> 
+  </section> 
+</body> 
+</html> 
+ 
---
+++ archivo.js
@@ -0,0 +1,19 @@
+var numero = "Hola Mundo!!!!";
+alert(numero + "\n\r me la pelan jijijija");
+var xd = ["a", "b", "c"];
+alert(xd);
+
+var mivariable = 8; 
+switch(mivariable) { 
+  case 5: {
+    alert("El número es cinco"); 
+    break; }
+  case 8: {
+    alert("El número es ocho"); 
+    break; }
+  case 10: {
+    alert("El número es diez"); 
+    break; }
+  default: {
+    alert("El número es " + mivariable); }
+}
